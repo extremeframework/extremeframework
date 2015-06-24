342c3a9531f8ff9ce70f498c5145e319fb173961
Viet Tran <vtq@vietmaisolutions.com>
2015-06-24 22:53:31 +0700

    Add + delete files

--- admin/controller/AppController.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright (c) 2011-2015 VIETMAI Solutions Corporation
- *
- * Released under the MIT license (http://opensource.org/licenses/MIT)
- */
-class AppController extends __AppController {
-}
