22b6f81c78e726c3d69448e03bf0a6581cdce746
Viet Tran <vtq@vietmaisolutions.com>
2015-06-28 12:00:09 +0700

    Composer autoload + remove license check

--- shared/index.php
+++ shared/index.php
@@ -129,8 +129,5 @@ function application_route($url = null) {
     exit(0);
 }
 
-// Apply handshake plugin
-LicenseCheckHelper::apply_handshake_plugin();
-
 // Route application
 application_route();
