2630eae0b787f06dcdb1dd7f2531a885fc2ae867
Viet Tran <vtq@vietmaisolutions.com>
2015-10-10 09:52:24 +0700

    enforce height only for images

--- library/external/Smarty/libs/plugins/function.media.php
+++ library/external/Smarty/libs/plugins/function.media.php
@@ -11,8 +11,8 @@ function smarty_function_media($params, &$smarty)
 
 	$thumbnail = isset($params['thumbnail'])? $params['thumbnail'] : false;
 	$linkicon = isset($params['linkicon'])? $params['linkicon'] : false;
-	$width = isset($params['width'])? $params['width'] : THUMBS_WIDTH;
-	$height = isset($params['height'])? $params['height'] : THUMBS_HEIGHT;
+	$width = isset($params['width'])? $params['width'] : 0;
+	$height = isset($params['height'])? $params['height'] : 0;
 	$class = isset($params['class'])? $params['class'] : '';
 
     if (!empty($class)) {
@@ -26,12 +26,17 @@ function smarty_function_media($params, &$smarty)
             $remote = preg_match('/http/i', $src);
 
             if ($thumbnail && !$remote) {
+                $width = $width? $width : THUMBS_WIDTH;
+                $height = $height? $height : THUMBS_HEIGHT;
+
                 $html = '<a class="fancybox" href="'.UPLOAD_URL.'/'.$src.'" target="blank"><img '.$class.'src="'.APPLICATION_URL.'/thumb/'.$width.'x'.$height.'/'.$src.'"/></a>';
             } else {
+                $height = $height? $height : THUMBS_HEIGHT;
+
                 if ($remote) {
-                    $html = '<img '.$class.'src="'.$src.'" width="'.$width.'" height="'.$height.'"/>';
+                    $html = '<img '.$class.'src="'.$src.'" height="'.$height.'"/>';
                 } else {
-                    $html = '<img '.$class.'src="'.UPLOAD_URL.'/'.$src.'" width="'.$width.'" height="'.$height.'"/>';
+                    $html = '<img '.$class.'src="'.UPLOAD_URL.'/'.$src.'" height="'.$height.'"/>';
                 }
             }
         } else if (preg_match('/(flv|swf)/is', $extension)) {
@@ -52,7 +57,9 @@ function smarty_function_media($params, &$smarty)
     } elseif (preg_match('/youtube\.com\/watch\?v=([a-z0-9]+)/is', $src, $matches)) {
         $html = '<object '.$class.' style="height: 390px; width: 640px"><param name="movie" value="http://www.youtube.com/v/'.$matches[1].'?version=3&feature=player_embedded"><param name="allowFullScreen" value="true"><param name="allowScriptAccess" value="always"><embed src="http://www.youtube.com/v/'.$matches[1].'?version=3&feature=player_embedded" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always" width="640" height="360"></object>';
     } else {
-    	$html = '<img '.$class.'src="'.$src.'" width="'.$width.'" height="'.$height.'"/>';
+        $height = $height? $height : THUMBS_HEIGHT;
+
+    	$html = '<img '.$class.'src="'.$src.'" height="'.$height.'"/>';
     }
 
     if ($linkicon) {
